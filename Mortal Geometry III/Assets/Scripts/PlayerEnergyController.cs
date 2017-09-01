d08c46a934d359491a9a84322c4bcd1672ebf19c
ZecaRodrigues <30570179+ZecaRodrigues@users.noreply.github.com>
2017-09-01 19:16:11 +0100

    01092017

---
+++ Mortal Geometry III/Assets/Scripts/PlayerEnergyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerEnergyController : MonoBehaviour {
+
+	public float energyMax = 1f;
+	public float energyCurrent = 0f;
+
+	public PlayerMovement movement;
+
+	void Start () {
+
+		energyCurrent = energyMax;
+
+	}
+
+	void Update () {
+
+		if(energyCurrent <= 0)
+		{
+			movement.enabled = false;
+			FindObjectOfType<GameAdmin> ().EndGame();
+
+		}
+			
+		GetComponent<MeshRenderer> ().material.color = new Color (GetComponent<MeshRenderer> ().material.color.r, GetComponent<MeshRenderer> ().material.color.g, GetComponent<MeshRenderer> ().material.color.b, energyCurrent);
+
+	}
+}
---
+++ Mortal Geometry III/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+	//private Rigidbody rBody;
+	public float fowardForce = 4000f;
+	public float sidewaysForce = 500f;
+
+	public Rigidbody rB;
+	public Transform targetView;
+
+
+	void Start () {
+
+	//	rBody = GetComponent<Rigidbody> ();
+
+
+	}
+
+	void FixedUpdate () {
+
+	//	rBody.AddForce (0, 0, fowardForce * Time.deltaTime);
+
+	//	float inputX = Input.GetAxis ("Horizontal");
+	//	float inputZ = Input.GetAxis ("Vertical");
+
+	//	float moveX = inputX * fowardForce * Time.deltaTime;
+	//	float moveZ = inputZ * sidewaysForce * Time.deltaTime;
+
+	//	rBody.AddForce(moveX, 0f, moveZ);
+
+
+
+
+
+
+		rB.AddForce (0, 0, fowardForce * Time.deltaTime);
+	
+		if (Input.GetKey("d")) {
+
+			rB.AddForce (sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (Input.GetKey("a")) {
+
+			rB.AddForce (-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+		}
+
+		if (rB.position.y < -1f) {
+		
+			FindObjectOfType<GameAdmin> ().EndGame ();
+		
+		}
+
+		transform.LookAt (targetView);
+
+
+	}
+}
